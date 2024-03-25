39fb413d370b3f7feaa0770ab85d216692829b81
Aung Zaw Paing Oo <aungzawpaingoonetwork@gmail.com>
2024-03-25 16:00:45 +0630

    Day_0_100%_Done

--- Project_Js_Day_0/assets/scripts/app.js
+++ Project_Js_Day_0/assets/scripts/app.js
@@ -3,16 +3,75 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
+function getUserInput() {
+
+  return parseInt(userInput.value)  
+
+}
+
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+
+     const calDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+     outputResult(currentResult, calDescription);
+
+
+}
+
+
+
+
 function add () {
 
- const calDescription = `${currentResult} + ${userInput.value}`   
- currentResult = currentResult + + userInput.value;
- outputResult(currentResult,calDescription);
+ const enteredNumber = getUserInput()
+ const initialResult = currentResult;
+ currentResult = currentResult + + enteredNumber;
+ createAndWriteOutput('+', initialResult, enteredNumber );
+
+}
+
+
+function subtract () {
+  
+    const enteredNumber = getUserInput()
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber );
+
+}
+
+
+function multiply () {
+  
+    const enteredNumber = getUserInput()
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber );
+
+}
+
+
+function divide () {
+
+    const enteredNumber = getUserInput()
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber );
 
 }
 
 
 addBtn.addEventListener('click', add);
+
+subtractBtn.addEventListener('click',subtract);
+
+multiplyBtn.addEventListener('click',multiply);
+
+divideBtn.addEventListener('click',divide);
+
+
+
+
 
 
 
