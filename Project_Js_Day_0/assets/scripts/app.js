706285a636a8b8d26abc181ca4885a51143c0cd8
Aung Zaw Paing Oo <aungzawpaingoonetwork@gmail.com>
2024-03-25 11:21:00 +0630

    Day_0_50%_Done

--- Project_Js_Day_0/assets/scripts/app.js
+++ Project_Js_Day_0/assets/scripts/app.js
@@ -1,1 +1,10 @@
-alert ("Hello World");
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1   ;
+
+let calculationDescription = `(${defaultResult} + 10 ) * 3 / 2 - 1` ;
+
+
+outputResult(currentResult,calculationDescription);
