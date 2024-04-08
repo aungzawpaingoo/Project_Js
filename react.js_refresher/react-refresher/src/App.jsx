c49d47a1508047ce1fa2bc6142938e78da43f4a2
Aung Zaw Paing Oo <aungzawpaingoo@yomabank.com>
2024-04-08 16:53:29 +0630

    'Started'

---
+++ react.js_refresher/react-refresher/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+
+    <div>
+      <h1>React Refresher</h1>
+      <div className="card">
+        <h3>Title</h3>
+        <button>Delete</button>
+      </div>
+    </div>
+   
+  );
+}
+
+export default App;
